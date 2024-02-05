e5ec846363ec977b773caa0f08975a437b1d468a
mauliksinh2003 <144231047+mauliksinh2003@users.noreply.github.com>
2024-02-05 11:59:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import React, { useState } from 'react';
+import './App.css';
+import Navbar from './Navbar';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import Card from 'react-bootstrap/Card';
+
+function App() {
+ 
+  return (
+  <div>
+    <Navbar /> 
+  </div>
+  )
+}
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+function Navbar() {
+
+  return (
+    <nav>
+      <div className="Navbar"> 
+      <ul>
+        <li><a href="#">Homepage</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Contact us</a></li>
+      </ul>
+      </div>
+    </nav>
+  )
+
+}
+export default Navbar;
